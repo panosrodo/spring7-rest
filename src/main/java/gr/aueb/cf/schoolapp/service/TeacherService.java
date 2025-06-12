8a782cba4ffc1b694c69a1c5fdb7d01a982fdb93
panosrodo <brandonroots@hotmail.gr>
2025-06-12 19:56:55 +0300

    feat: add filtering logic

--- src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
@@ -2,6 +2,9 @@ package gr.aueb.cf.schoolapp.service;
 
 import gr.aueb.cf.schoolapp.core.exceptions.AppObjectAlreadyExists;
 import gr.aueb.cf.schoolapp.core.exceptions.AppObjectInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.filters.Paginated;
+import gr.aueb.cf.schoolapp.core.filters.TeacherFilters;
+import gr.aueb.cf.schoolapp.core.specifications.TeacherSpecification;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.mapper.Mapper;
@@ -18,6 +21,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.springframework.web.multipart.MultipartFile;
@@ -102,8 +106,17 @@ public class TeacherService {
         return teacherRepository.findAll(pageable).map(mapper::mapToTeacherReadOnlyDTO);
     }
 
+    @Transactional
+    public Paginated<TeacherReadOnlyDTO> getTeachersFilteredPaginated(TeacherFilters filters) {
+        var filtered = teacherRepository.findAll(getSpecsFromFilters(filters), filters.getPageable());
+        return new Paginated<>(filtered.map(mapper::mapToTeacherReadOnlyDTO));
+    }
 
-
-
-
+    private Specification<Teacher> getSpecsFromFilters(TeacherFilters filters) {
+        return Specification
+                .where(TeacherSpecification.teacherStringFieldLike("uuid", filters.getUuid()))
+                .and(TeacherSpecification.teacherUserAfmIs(filters.getUserAfm()))
+                .and(TeacherSpecification.teacherPersonalInfoAmkaIs(filters.getUserAmka()))
+                .and(TeacherSpecification.teacherIsActive(filters.getIsActive()));
+    }
 }
